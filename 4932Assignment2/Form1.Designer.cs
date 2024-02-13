97257c171e4fdaf661e07979bdcc26759fdeacc6
rohilpatel15 <rohilpatel123@outlook.com>
2024-02-13 15:58:00 -0800

    Add project files.

---
+++ 4932Assignment2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace _4932Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 31);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(46, 28);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    }
+}
+
---
+++ 4932Assignment2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4932Assignment2
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap loadedBMap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (loadedBMap != null)
+            {
+                // Draw the image onto the form
+                e.Graphics.DrawImage(loadedBMap, Point.Empty);
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                loadedBMap = new Bitmap(openFileDialog.FileName);
+                loadedBMap = new Bitmap(loadedBMap, loadedBMap.Width, loadedBMap.Height);
+                Refresh();
+            }
+        }
+
+        private void ConvertToYCbCr(Bitmap bmp)
+        {
+            float[,] Y = new float[bmp.Width, bmp.Height];
+            float[,] Cb = new float[bmp.Width, bmp.Height];
+            float[,] Cr = new float[bmp.Width, bmp.Height];
+
+            // Loop through each pixel in the bitmap
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color pixel = bmp.GetPixel(x, y);
+
+                    // Extract RGB values
+                    float R = pixel.R;
+                    float G = pixel.G;
+                    float B = pixel.B;
+
+                    // Convert to YCrCb
+                    Y[x, y] = 0.299f * R + 0.587f * G + 0.114f * B;
+                    Cb[x, y] = (-0.168736f * R) + (-0.331264f * G) + (-0.5f * B) + 128;
+                    Cb[x, y] = (0.5f * R) + (-0.418688f * G) + (-0.081312f * B) + 128;
+                    Cr[x, y] = (0.5f * R) + (-0.418688f * G) + (-0.081312f * B) + 128;
+                }
+            }
+        }
+
+        private void ConvertFromYCbCr(Bitmap bmp)
+        {
+            float[,] Y = new float[bmp.Width, bmp.Height];
+            float[,] Cb = new float[bmp.Width / 2, bmp.Height / 2];
+            float[,] Cr = new float[bmp.Width / 2, bmp.Height / 2];
+
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    // Perform necessary operations to convert YCrCb to RGB
+                    float Y_val = Y[x, y];
+                    float Cb_val = Cb[x / 2, y / 2];
+                    float Cr_val = Cr[x / 2, y / 2];
+
+                    // Convert YCrCb to RGB
+                    float R = (1f * Y_val) + (0 * Cb_val) + (1.4f * Cr_val);
+                    float G = (1f * Y_val) + (-0.343f * Cb_val) + (-0.711f * Cr_val);
+                    float B = (1f * Y_val) + (1.765f * Cb_val) + (0 * Cr_val);
+
+                    // Ensure RGB values are within the valid range
+                    R = Math.Max(0, Math.Min(255, R));
+                    G = Math.Max(0, Math.Min(255, G));
+                    B = Math.Max(0, Math.Min(255, B));
+
+                    // Set RGB values in the bitmap
+                    bmp.SetPixel(x, y, Color.FromArgb((int)R, (int)G, (int)B));
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
